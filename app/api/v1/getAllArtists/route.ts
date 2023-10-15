66ffd06678e2d3c9d399e80d561fb18d47c87310
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-10-15 12:53:48 -0700

    Create route.ts

---
+++ app/api/v1/getAllArtists/route.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from "next/server";
+
+
+export const revalidate = 0;
+//export const dynamic = 'force-dynamic'
+import { supabaseAdmin } from "@/lib/site/constants";
+export async function GET(request: Request) {
+//  const { searchParams } = new URL(request.url);
+//  const userId = searchParams.get("userId");
+try {
+ if(request.method === 'GET') {
+    const { data: artists, error } = await supabaseAdmin
+    .from('artists')
+    .select()
+    //.eq('student_id', userId)
+   // .limit(5)
+    console.log(artists)
+
+    if (error) {
+      throw new Error("Error fetching drops");
+    }    
+          const response = {
+             artists
+          };
+          await new Promise((resolve) => setTimeout(resolve, 500));
+          return  NextResponse.json(response);
+        }
+        } catch (error) {
+          console.error("Error fetching metadata:", error);
+          return NextResponse.json("Error: fetching metadata");
+        }
+
+        return NextResponse.json('Error: Method not found')
+      }
+
+
