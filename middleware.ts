7ce8cbb50d1e1f0c54ef7a851d1d099da55f5633
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-10-15 13:54:42 -0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -6,6 +6,16 @@ import type { NextRequest } from 'next/server'
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
+  const requestOrigin = req.headers.get('origin');
+  const allowedOrigins = ['https://cribnetwork.io', 'http://localhost:3000', 'https://cribmusic.xyz'];
+  // Check if the request's origin is in the allowed origins list
+  if (requestOrigin && allowedOrigins.includes(requestOrigin)) {
+    // If the origin is allowed, set the appropriate CORS headers
+    res.headers.set('Access-Control-Allow-Origin', requestOrigin);
+    res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.headers.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
+  }
+
   const supabase = createMiddlewareClient({ req, res })
   await supabase.auth.getSession()
   return res
