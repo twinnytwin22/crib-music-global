4e224bdfafffcab90935878882e8e6489abc8190
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-10-16 05:16:48 -0700

    Update route.ts

--- app/api/v1/addNewArtist/route.ts
+++ app/api/v1/addNewArtist/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { supabaseAdmin } from "@/lib/site/constants";
 
-export const dyanmic = 'force-dynamic'
+export const dynamic = 'force-dynamic'
 export const revalidate = 0;
 const headers = {
   'Access-Control-Allow-Origin': '*',
