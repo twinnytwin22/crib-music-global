e2e2cc55dea9a0c46c0635c6548c2d2d8a64b8b3
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-10-24 22:54:29 -0700

    Update ArtistRowHeader.tsx

--- ui/Headers/ArtistRowHeader/ArtistRowHeader.tsx
+++ ui/Headers/ArtistRowHeader/ArtistRowHeader.tsx
@@ -7,14 +7,14 @@ function ArtistRowHeader({artists}) {
     <div>
          <div className="flex gap-4 max-w-screen-2xl mx-auto w-full">
         {artists?.artists.map((artist, i) => { 
-          const url = `/artist_images/${artist?.image_url}`
-          const  href= `/artist/${artist?.artist_id}`
+          const url = `/artist_images/${artist.image_url!}`.trim()
+          const  href= `/artist/${artist.artist_id!}`
           return (
           
           <div
             key={i}
             className=" aspect-video bg-zinc-500 h-full object-cover w-full overflow-hidden rounded relative  border border-zinc-200 dark:border-zinc-800"
-          ><Link href={href}>
+          ><Link href={href} prefetch={false} >
             <Image
               alt={artist.artist_name}
               src={url}
@@ -22,10 +22,14 @@ function ArtistRowHeader({artists}) {
              width={800}
              height={450}
              priority
+             style={{
+              objectFit: 'cover',
+            }}
             />
             <h2 className="text-white absolute bottom-4 left-4 font-semibold text-lg0">
               {artist.artist_name}
-            </h2>   </Link>
+            </h2>  
+             </Link>
           </div>
        
         )})}
