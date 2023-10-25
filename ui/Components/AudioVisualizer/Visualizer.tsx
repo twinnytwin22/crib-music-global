c49ca15619ec8e7e79e0a78824e4e9825e3ce99b
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-10-25 07:56:11 -0700

    update search n visulizer

--- ui/Components/AudioVisualizer/Visualizer.tsx
+++ ui/Components/AudioVisualizer/Visualizer.tsx
@@ -2,66 +2,97 @@
 import { useSubportPlayer } from 'app/context/subport-player';
 import { usePlayerStore } from 'app/context/subport-player/PlayerLogic';
 import { useEffect, useRef } from 'react';
-import WaveSurfer from 'wavesurfer.js';
+///import WaveSurfer from 'wavesurfer.js';
+
+const formWaveSurferOptions = (ref: any) => ({
+  container: ref,
+  waveColor: "#eee",
+  progressColor: "#FCA5A5",
+  cursorColor: "OrangeRed",
+  barWidth: 3,
+  barRadius: 3,
+ // responsive: true,
+  height: 35,
+ // normalize: true,
+ // partialRender: true
+});
+
 
 function AudioVisualizer({ audioFile }) {
-  const waveformRef = useRef<any>(null);
-  const wavesurferRef = useRef<any>(null);
-  //const [audio, setAudio] = useState<any>(null)
-
-  // Access the player store state and actions
   const {
     isPlaying,
+    setIsPlaying,
     setCurrentTime,
     setDuration,
+    
   } = usePlayerStore();
   const { play, stop } = useSubportPlayer();
+  const waveformRef = useRef<any>(null);
+  const wavesurfer = useRef<any>(null);
+  const create = async () => {
+    const WaveSurfer = (await import("wavesurfer.js")).default;
+    const options = formWaveSurferOptions(waveformRef?.current);
+    wavesurfer.current = WaveSurfer.create(options);
+    wavesurfer.current.load(audioFile);
+    return wavesurfer?.current
+  };
+
+//   const { data, error } = useQuery({
+//     queryKey:['data'],
+//   queryFn:() => create(), 
+//   enabled: !!waveformRef.current, 
+//   onSuccess: (data) => {
+//       data.destroy();
+    
+// }} );
+
+  //const [audio, setAudio] = useState<any>(null)
+  // useEffect(() => {
+  //   create();
+  //   return () => {
+  //     if (wavesurfer.current) {
+  //       wavesurfer.current.destroy();
+  //     }
+  //   };
+  // }, []);
+
+
+  // Access the player store state and actions
 
   useEffect(() => {
-    if (!audioFile) return;
-
-    // Create Wavesurfer instance if it doesn't exist
-
-      const wavesurfer = WaveSurfer.create({
-        container: waveformRef.current,
-        waveColor: '#9CA3AF',
-        progressColor: '#4B5563',
-        barWidth: .4,
-        barHeight: 0.2,
-      });
-
-      wavesurferRef.current = wavesurfer;
-
-      // Add event listeners for Wavesurfer
-      wavesurfer.on('play', () => {
-        play();
-      });
-
-      wavesurfer.on('pause', () => {
-        stop();
-      });
-
-      wavesurfer.on('audioprocess', () => {
-        setCurrentTime(wavesurfer.getCurrentTime());
-      });
-
-      wavesurfer.on('ready', () => {
-        setDuration(wavesurfer.getDuration());
-      });
-
-      // Load the audio file
-      wavesurfer.load(audioFile);
- 
-
+    create();
     return () => {
-      // Stop and destroy the Wavesurfer instance on unmount
-      wavesurferRef.current.stop();
-      wavesurfer.destroy();
+      if (wavesurfer.current) {
+        wavesurfer.current.destroy();
+      }
     };
-  }, [audioFile, setCurrentTime, setDuration, isPlaying]);
+  }, [audioFile]); // Listen to changes in audioFile
 
+  useEffect(() => {
+    // Define a function to update play state based on isPlaying
+    const updatePlayState = () => {
+      if (isPlaying) {
+        wavesurfer.current?.play();
+      } else {
+        wavesurfer.current?.pause();
+      }
+    };
+
+    // Add an event listener to Wavesurfer to update play state
+    wavesurfer.current?.on('play', () => {
+      play()
+    });
+    wavesurfer.current?.on('stop', () => {
+      stop()
+    });
+
+    // Call the function to set the initial play state
+    updatePlayState();
+  }, [isPlaying]);
+
+ 
   return (
-    <div ref={waveformRef} className='w-full max-w-2xl h-fit'>
+    <div ref={waveformRef} className='w-full max-w-2xl '>
       {/* The waveform will be displayed here */}
     </div>
   );
--- ui/Components/SearchBar/SearchBar.tsx
+++ ui/Components/SearchBar/SearchBar.tsx
@@ -9,8 +9,8 @@ import { useEffect, useRef, useState } from "react";
 function SearchBar() {
     const router = useRouter();
 
-    const {data:artists} = useQuery({queryKey: ['artists'], queryFn: () => getAllArtists()})
-    const {data:songs} = useQuery({queryKey: ['songs'], queryFn: () => getAllSongs()})
+    const { data: artists } = useQuery({ queryKey: ['artists'], queryFn: () => getAllArtists() })
+    const { data: songs } = useQuery({ queryKey: ['songs'], queryFn: () => getAllSongs() })
 
     const [searchTerm, setSearchTerm] = useState<string>("");
     const [searchResults, setSearchResults] = useState<any>({
@@ -28,32 +28,32 @@ function SearchBar() {
             // Call the search function here
             search();
         } else {
-            setSearchResults({ artists: [], songs: []});
+            setSearchResults({ artists: [], songs: [] });
             setIsOpen((prevState) => !prevState);
         }
     }, [searchTerm, isInputFocused]);
-    console.log
+  //  console.log
     const search = () => {
         // Filter artists based on artist_name and genres
         const artistResults = artists.artists.filter((artist) => {
-          const artistName = artist.artist_name.toLowerCase();
-          const genres = artist.genres.map((genre) => genre.toLowerCase());
-          const searchTermLower = searchTerm.toLowerCase();
-          return artistName.includes(searchTermLower) || genres.includes(searchTermLower);
+            const artistName = artist.artist_name.toLowerCase();
+            const genres = artist.genres.map((genre) => genre.toLowerCase());
+            const searchTermLower = searchTerm.toLowerCase();
+            return artistName.includes(searchTermLower) || genres.includes(searchTermLower);
         });
-      
+
         // Filter songs based on artist_name and genres
         const songResults = songs.songs.filter((song) => {
-          const artistName = song.artist_name.toLowerCase();
-          const genres = song.genres.map((genre) => genre.toLowerCase());
-          const searchTermLower = searchTerm.toLowerCase();
-          return artistName.includes(searchTermLower) || genres.includes(searchTermLower);
+            const artistName = song.artist_name.toLowerCase();
+            const genres = song.genres.map((genre) => genre.toLowerCase());
+            const searchTermLower = searchTerm.toLowerCase();
+            return artistName.includes(searchTermLower) || genres.includes(searchTermLower);
         });
-      
+
         setSearchResults({ artists: artistResults, songs: songResults });
         setIsOpen(true); // Open the search results dropdown
-      };
-          const handleInputFocus = () => {
+    };
+    const handleInputFocus = () => {
         setIsOpen((prevState) => !prevState);
     };
 
@@ -68,103 +68,105 @@ function SearchBar() {
         setIsOpen((prevState) => !prevState);
     };
 
-    console.log(searchResults)
+   // console.log(searchResults)
     return (
         <div>
-        <form className="block lg:pl-2 w-72 md:w-96">
-            <div className="relative z-0">
-                <input
-                    autoComplete="off"
-                    type="text"
-                    id="default_standard"
-                    className="block py-2.5 px-0 w-full text-sm text-zinc-900 bg-transparent border-0 border-b-2 border-zinc-300 appearance-none dark:text-white dark:border-zinc-600 dark:focus:border-red-300 focus:outline-none focus:ring-0 focus:border-red-300 peer"
-                    placeholder=" "
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                    required
-                    onFocus={handleInputFocus} // Add onFocus and onBlur event handlers
-                    //   onBlur={handleInputBlur}
-                    ref={searchInputRef}
-                />
-                <label
-                    htmlFor="default_standard"
-                    className="absolute text-sm text-zinc-500 dark:text-zinc-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-red-300 peer-focus:dark:text-red-300 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >
-                    Search Sounds
-                </label>
-            </div>
-        </form>
-        
-        {isOpen && (
-        <div className="absolute max-w-md w-full top-10 left-0  mt-6  bg-white max-h-[300px] overflow-y-scroll dark:bg-zinc-900 border border-zinc-200 dark:border-zinc-800 shadow-2xl rounded-md z-[99990] shadow-zinc-300 dark:shadow-black search-results mx-auto">
-          {searchResults?.artists?.length > 0 && (
-            <div className="relative">
-              <div className="p-1 pl-4 bg-white dark:bg-black w-full">
-                <h1 className="text-sm font-bold">Artists</h1>
-              </div>
-              {searchResults.artists.map((artist: any) => { 
-    const imageUrl = `/artist_images/${artist.image_url!}`.trim();
-    const href = ''
-                return (
-                <div
-                  onClick={() => handleLink(`/artist/${artist.artist_id}`)}
-                  key={artist.artist_id}
-                  className="flex items-center p-2.5 border-b border-zinc-300 dark:border-zinc-800 hover:bg-zinc-100 dark:hover:bg-zinc-800 relative z-[99999] cursor-pointer"
-                >
-                  <Image
-                    width={100}
-                    height={100}
-                    src={imageUrl}
-                    alt={artist?.artist_name|| ''}
-                    className="w-8 h-8 rounded-full mr-2 aspect-square object-cover"
-                  />
-                  <div>
-                    <p className="text-black dark:text-white font-medium">
-                      {artist.artist_name}
-                    </p>
-                    <div className="text-gray-500 dark:text-gray-400 text-sm">
-                      {artist?.email}
-                    </div>
-                  </div>
+            <form className="block lg:pl-2 w-72 md:w-96">
+                <div className="relative z-0">
+                    <input
+                        autoComplete="off"
+                        type="text"
+                        id="default_standard"
+                        className="block py-2.5 px-0 w-full text-sm text-zinc-950 bg-transparent border-0 border-b-2 border-zinc-300 appearance-none dark:text-white dark:border-zinc-600 dark:focus:border-red-300 focus:outline-none focus:ring-0 focus:border-red-300 peer"
+                        placeholder=" "
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                        required
+                        onFocus={handleInputFocus} // Add onFocus and onBlur event handlers
+                        //   onBlur={handleInputBlur}
+                        ref={searchInputRef}
+                    />
+                    <label
+                        htmlFor="default_standard"
+                        className="absolute text-sm text-zinc-500 dark:text-zinc-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-red-300 peer-focus:dark:text-red-300 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                    >
+                        Search Sounds
+                    </label>
                 </div>
-              )})}
-            </div>
-          )}
+            </form>
 
-          {searchResults?.songs?.length > 0 && (
-            <div className="mt-4">
-              <div className="p-1 pl-4 bg-white dark:bg-black">
-                <h1 className="text-sm font-bold">Songs</h1>
-              </div>
-              {searchResults.songs.map((song: any) => {
-    const imageUrl = `/song_covers/${song.cover_art_url!}`.trim();
-    const href = ''
-                return  (
-                <div
-                  onClick={() => handleLink(`/song/${song.song_id}`)}
-                  key={song.song_id}
-                  className="flex items-center p-2 border-b border-zinc-300 dark:border-zinc-800 hover:bg-zinc-100 dark:hover:bg-zinc-800  cursor-pointer"
-                >
-                  <Image
-                    width={100}
-                    height={100}
-                    src={imageUrl}
-                    alt={song.title}
-                    className="w-8 h-8 rounded-full mr-2 aspect-square object-cover"
-                  />
-                  <div>
-                    <p className="text-black dark:text-white font-medium">
-                      {song.title}
-                    </p>
-                    <div className="text-gray-500 dark:text-gray-400 text-sm">
-                      {song?.location}
-                    </div>
-                  </div>
-                </div>
-              )})}
-            </div>
-          )}
-{/* 
+            {isOpen && (
+                <div className="absolute max-w-md w-full top-10 left-0  mt-6  bg-white max-h-[300px] overflow-y-scroll dark:bg-zinc-950 border border-zinc-200 dark:border-zinc-800 shadow-2xl rounded-md z-[99990] shadow-zinc-300 dark:shadow-black search-results mx-auto">
+                    {searchResults?.artists?.length > 0 && (
+                        <div className="relative">
+                            <div className="p-1 pl-4 bg-zinc-100 dark:bg-black w-full">
+                                <h1 className="text-sm font-bold">Artists</h1>
+                            </div>
+                            {searchResults.artists.map((artist: any) => {
+                                const imageUrl = `/artist_images/${artist.image_url!}`.trim();
+                                const href = ''
+                                return (
+                                    <div
+                                        onClick={() => handleLink(`/artist/${artist.artist_id}`)}
+                                        key={artist.artist_id}
+                                        className="flex items-center p-2.5 border-b border-zinc-300 dark:border-zinc-800 hover:bg-zinc-100 dark:hover:bg-zinc-800 relative z-[99999] cursor-pointer"
+                                    >
+                                        <Image
+                                            width={100}
+                                            height={100}
+                                            src={imageUrl}
+                                            alt={artist?.artist_name || ''}
+                                            className="w-8 h-8 rounded-full mr-2 aspect-square object-cover"
+                                        />
+                                        <div>
+                                            <p className="text-black dark:text-white font-medium text-sm">
+                                                {artist.artist_name}
+                                            </p>
+                                            <div className="text-zinc-500 dark:text-zinc-400 text-sm">
+                                                {artist?.email}
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    )}
+
+                    {searchResults?.songs?.length > 0 && (
+                        <div className="mt-4">
+                            <div className="p-1 pl-4 bg-zinc-100 dark:bg-black">
+                                <h1 className="text-sm font-bold">Songs</h1>
+                            </div>
+                            {searchResults.songs.map((song: any) => {
+                                const imageUrl = `/song_covers/${song.cover_art_url!}`.trim();
+                                const href = ''
+                                return (
+                                    <div
+                                        onClick={() => handleLink(`/song/${song.song_id}`)}
+                                        key={song.song_id}
+                                        className="flex items-center p-2 border-b border-zinc-300 dark:border-zinc-800 hover:bg-zinc-100 dark:hover:bg-zinc-800  cursor-pointer"
+                                    >
+                                        <Image
+                                            width={100}
+                                            height={100}
+                                            src={imageUrl}
+                                            alt={song.title}
+                                            className="w-8 h-8 rounded-full mr-2 aspect-square object-cover"
+                                        />
+                                        <div>
+                                            <p className="text-black dark:text-white font-medium text-sm">
+                                                {song.title}
+                                            </p>
+                                            <div className="text-zinc-500 dark:text-zinc-400 text-sm">
+                                                {song?.location}
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    )}
+                    {/* 
           {searchResults?.drops?.length > 0 && (
             <div className="mt-4">
               <div className="p-1 pl-4 bg-white dark:bg-black">
@@ -187,7 +189,7 @@ function SearchBar() {
                     <div className="text-black dark:text-white font-medium">
                       {drop.title}
                     </div>
-                    <div className="text-gray-500 dark:text-gray-400 text-sm">
+                    <div className="text-zinc-500 dark:text-zinc-400 text-sm">
                       {drop?.genres?.toString()}
                     </div>
                   </div>
@@ -195,8 +197,8 @@ function SearchBar() {
               ))}
             </div>
           )} */}
-        </div>
-      )}
+                </div>
+            )}
 
         </div>
     );
