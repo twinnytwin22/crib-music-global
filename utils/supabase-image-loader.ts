d1d9444e5dd9efd8eef82fb7fa94d51508d0e610
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-10-24 22:26:52 -0700

    Update supabase-image-loader.ts

--- utils/supabase-image-loader.ts
+++ utils/supabase-image-loader.ts
@@ -1,7 +1,7 @@
 const projectId = 'tvuqvrbxusmicpmjqpus' // your supabase project id
 
 export default function supabaseLoader({ src, width, quality }) {
-  return `https://${projectId}.supabase.co/storage/v1/render/image/public/${src}?quality=${
+  return `https://${projectId}.supabase.co/storage/v1/render/image/public${src}?quality=${
     quality || 75
   }`
 }
