f86c57023d468c7045d6cf20ce909681ffade7a8
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-11-16 11:10:27 -0700

    Update LicenseButton.tsx

--- ui/Buttons/LicenseButton/LicenseButton.tsx
+++ ui/Buttons/LicenseButton/LicenseButton.tsx
@@ -20,7 +20,7 @@ function LicenseButton({ id, song }) {
   return (
     <React.Fragment>
       <Suspense>
-      {id! && song.length > 1 &&
+      {id! && song! &&
     <button
       onClick={handleSetId}
       className="bg-zinc-100 dark:hover:bg-zinc-950 dark:bg-black hover:bg-zinc-200 border border-zinc-200 dark:border-zinc-700 text-primary-800 ease-in-out duration-300 text-xs font-medium px-2 py-1.5 rounded"
