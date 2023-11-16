206a5363cb08513b87e77fef921577cd99448ca4
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-11-16 11:07:35 -0700

    Update LicenseButton.tsx

--- ui/Buttons/LicenseButton/LicenseButton.tsx
+++ ui/Buttons/LicenseButton/LicenseButton.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { createQueryString } from "@/lib/hooks/createQueryString";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import React, { Suspense } from "react";
 import { useLicensingStore } from "./LicenseButtonStore";
 
 function LicenseButton({ id, song }) {
@@ -17,12 +18,17 @@ function LicenseButton({ id, song }) {
   };
 
   return (
+    <React.Fragment>
+      <Suspense>
+      {id! && song.length > 1 &&
     <button
       onClick={handleSetId}
       className="bg-zinc-100 dark:hover:bg-zinc-950 dark:bg-black hover:bg-zinc-200 border border-zinc-200 dark:border-zinc-700 text-primary-800 ease-in-out duration-300 text-xs font-medium px-2 py-1.5 rounded"
     >
       License
-    </button>
+    </button>}
+    </Suspense>
+    </React.Fragment>
   );
 }
 
